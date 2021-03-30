9686e0ff434dbe3cfd7c2e0d7a951186e28a80d5
ZXyy2627 <1375423395@qq.com>
2021-03-30 22:59:31 +0800

    什么是等待通知机制？ 就是当线程a处于等待状态时，另一个线程b改变了某些东西后，会通知线程a开始执行代码

---
+++ src/main/java/chap21_multiThread/multiThreadCommunicate/WaitNotifyMechanism_2.java
@@ -0,0 +1,9 @@
+package chap21_multiThread.multiThreadCommunicate;
+
+/**
+ * mechanism 机制
+ * 什么是等待/通知机制？
+ * 就是一个线程处于等待状态，另一个线程改变了某些东西的时候，会通知正在等待的线程可以开始干活了
+ */
+public class WaitNotifyMechanism_2 {
+}
